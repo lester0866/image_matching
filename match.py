1e7af03382b214162fbc656b9b0353be68cd07fa
lester <lester0866@gmail.com>
2019-11-21 19:40:00 -0500

    update_

--- match.py
+++ match.py
@@ -71,40 +71,47 @@ orb = cv2.ORB_create()
 kp1, des1 = orb.detectAndCompute(img1, None)
 proj_centroid = np.array([0, 0])
 frame_counter = 0
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 while True:
-    # try:
-    _, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    kp2, des2 = orb.detectAndCompute(frame, None)
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    # Match descriptors.
-    good_matches = bf.match(des1, des2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-    centroid = (dst_pts.sum(0) / len(dst_pts)).squeeze().astype('int')
-    frame = cv2.circle(frame, tuple(centroid), 10, (255, 255, 255), -1)
-    offset = np.array([cam_w // 2, cam_h // 2]) - centroid
-    curr_centroid = np.array([proj_size // 2, proj_size // 2]) - offset
-    if np.sum(np.abs(curr_centroid - proj_centroid)) >= 50:
-        proj_centroid = curr_centroid
-        x1, y1 = proj_centroid - proj_size // 4
-        x2, y2 = proj_centroid + proj_size // 4
-    projector = np.zeros((proj_size, proj_size, 3), dtype=np.uint8)
-    _, video_fr = cap.read()
-    video_fr = cv2.resize(video_fr, (proj_size // 2, proj_size // 2))
-    projector[int(y1):int(y2), int(x1):int(x2):] = video_fr
-    cv2.imshow('show1', projector)
-    cv2.imshow('show2', frame)
-    k = cv2.waitKey(20)
-    frame_counter += 1
-    if k % 256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        frame_counter = 0
-        cap = cv2.VideoCapture(play.url)
-    # except cv2.error as e:
-    #     print('distance error')
+    try:
+        _, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        kp2, des2 = orb.detectAndCompute(frame, None)
+
+        # Match descriptors.
+        if des2 is not None:
+            good_matches = bf.match(des1, des2)
+            distance_sum = int(sum([m.distance for m in good_matches]))
+
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            centroid = (dst_pts.sum(0) / len(dst_pts)).squeeze().astype('int')
+            frame = cv2.circle(frame, tuple(centroid), 10, (255, 255, 255), -1)
+            offset = np.array([cam_w // 2, cam_h // 2]) - centroid
+            curr_centroid = np.array([proj_size // 2, proj_size // 2]) - offset
+            if np.sum(np.abs(curr_centroid - proj_centroid)) >= 50 and distance_sum >= 3000:
+                proj_centroid = curr_centroid
+                x1, y1 = proj_centroid - proj_size // 8
+                x2, y2 = proj_centroid + proj_size // 8
+                print(distance_sum)
+        projector = np.zeros((proj_size, proj_size, 3), dtype=np.uint8)
+        _, video_fr = cap.read()
+        video_fr = cv2.resize(video_fr, (proj_size // 4, proj_size // 4))
+        projector[int(y1):int(y2), int(x1):int(x2):] = video_fr
+        cv2.imshow('show1', projector)
+        cv2.imshow('show2', frame)
+        k = cv2.waitKey(20)
+        frame_counter += 1
+        if k % 256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+        if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            frame_counter = 0
+            cap = cv2.VideoCapture(play.url)
+    except cv2.error as e:
+
+        print('distance error')
+        print(e)
 
 cam.release()
 cap.release()
